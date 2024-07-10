f5a08b586b90555828e993e1b85a84d65ee86d31
tottan <t_izaki@outlook.jp>
2024-07-11 01:33:21 +0900

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
